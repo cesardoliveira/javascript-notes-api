b837dcf2fb36c640253724b06fea1d55fdce9bc3
Cesar Augusto <cesarsdoliveira@gmail.com>
2020-09-15 15:36:30 +1000

    Using the library bcrypt to hash passwords

--- app/models/user.js
+++ app/models/user.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 let userSchema = new mongoose.Schema({
     name: String,
-    email: { type: String, require: true, unique: true},
-    password: { type: String, require: true},
+    email: { type: String, require: true, unique: true },
+    password: { type: String, require: true },
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now }
+});
+
+userSchema.pre('save', function (next) {
+    if (this.isNew || this.isModified('password')) {
+        bcrypt.hash(this.password, 10, (error, hashPassword) => {
+            if (error)
+                next(error)
+            else {
+                this.password = hashPassword;
+                next();
+            }
+        })
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
